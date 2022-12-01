d6cfb39780c9ca2fd6c8b61c3f7267a651409ffd
Füredi Dániel <daniel.furedi03@gmail.com>
2022-12-01 15:28:11 +0100

    adat insert, read kesz sqlite helper class kesz

--- app/src/main/java/hu/petrik/android_12_01/InsertActivity.java
+++ app/src/main/java/hu/petrik/android_12_01/InsertActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import hu.petrik.android_12_01.databinding.ActivityInsertBinding;
 import hu.petrik.android_12_01.databinding.ActivityMainBinding;
@@ -13,6 +14,8 @@ public class InsertActivity extends AppCompatActivity {
 
     ActivityInsertBinding binding;
 
+    private DBHelper db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +23,27 @@ public class InsertActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         addEventListeners();
+        db = new DBHelper(InsertActivity.this);
     }
 
     private void addEventListeners() {
         binding.insertInsertButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                String vezetekNev = binding.veznev.getText().toString().trim();
+                String keresztNev = binding.kernev.getText().toString().trim();
+                String jegyString = binding.jegy.getText().toString().trim();
+                if (vezetekNev.isEmpty() || keresztNev.isEmpty() || jegyString.isEmpty()){
+                    Toast.makeText(InsertActivity.this, "Minden mező kitöltése kötelező", Toast.LENGTH_SHORT).show();
+                } else {
+                    int jegy = Integer.parseInt(jegyString);
+                    // TODO: jegy validation
+                    if (db.rogzites(vezetekNev, keresztNev, jegy)){
+                        Toast.makeText(InsertActivity.this, "Sikeres adatrögzítés", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(InsertActivity.this, "Sikertelen adatrögzítés", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
         binding.insertBackButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/hu/petrik/android_12_01/MainActivity.java
+++ app/src/main/java/hu/petrik/android_12_01/MainActivity.java
@@ -3,14 +3,18 @@ package hu.petrik.android_12_01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.Toast;
 
 import hu.petrik.android_12_01.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    private DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +23,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         addEventListeners();
+        db = new DBHelper(MainActivity.this);
     }
 
     private void addEventListeners() {
+        binding.resultTextView.setMovementMethod(new ScrollingMovementMethod());
+
         binding.dataReadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Cursor adatok = db.listaz();
+                if (adatok.getCount() == 0){
+                    Toast.makeText(MainActivity.this, "Nincs az adatbázisban bejegyzés", Toast.LENGTH_SHORT).show();
+                } else {
+                    StringBuilder bobTheBuilder = new StringBuilder();
+                    while (adatok.moveToNext()){
+                        bobTheBuilder.append("ID: ").append(adatok.getInt(0));
+                        bobTheBuilder.append(System.lineSeparator());
+                        bobTheBuilder.append("Vezetéknév: ").append(adatok.getString(1));
+                        bobTheBuilder.append(System.lineSeparator());
+                        bobTheBuilder.append("Keresztnév: ").append(adatok.getString(2));
+                        bobTheBuilder.append(System.lineSeparator());
+                        bobTheBuilder.append("Jegy: ").append(adatok.getInt(3));
+                        bobTheBuilder.append(System.lineSeparator());
+                        bobTheBuilder.append(System.lineSeparator());
+                    }
+                    binding.resultTextView.setText(bobTheBuilder.toString());
+                }
             }
         });
         binding.dataInsertButton.setOnClickListener(new View.OnClickListener() {
