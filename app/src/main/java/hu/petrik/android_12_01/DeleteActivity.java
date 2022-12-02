5fb81fb7bdd13fa7bf586a7e9db750acae6dd9b8
Füredi Dániel <daniel.furedi03@gmail.com>
2022-12-02 09:09:04 +0100

    delete activity es code kesz van minden kesz

--- app/src/main/java/hu/petrik/android_12_01/DeleteActivity.java
+++ app/src/main/java/hu/petrik/android_12_01/DeleteActivity.java
@@ -2,13 +2,48 @@ package hu.petrik.android_12_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import hu.petrik.android_12_01.databinding.ActivityDeleteBinding;
 
 public class DeleteActivity extends AppCompatActivity {
+    
+    ActivityDeleteBinding binding;
+    DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_delete);
+        binding = ActivityDeleteBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        
+        db = new DBHelper(DeleteActivity.this);
+        
+        binding.buttonDeleteDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = binding.editTextIdDelete.getText().toString().trim();
+                if (id.isEmpty()){
+                    Toast.makeText(DeleteActivity.this, "A mező kitöltése kötelező!", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (db.torles(id) == 0){
+                        Toast.makeText(DeleteActivity.this, "Sikertelen törlés, nincs ilyen id", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(DeleteActivity.this, "Sikeres törlés!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        binding.buttonBackDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DeleteActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
